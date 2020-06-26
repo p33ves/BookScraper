f36a296e8ee7ffded38eb22cc55df0f31bc36c39
Vignesh P <vignesh.p1110@gmail.com>
2020-06-26 21:50:41 +0530

    exploring css selectors

--- amazon_books/amazon_books/spiders/amazon_spider.py
+++ amazon_books/amazon_books/spiders/amazon_spider.py
@@ -3,7 +3,13 @@ import scrapy
 
 class AmazonSpider(scrapy.Spider):
     name = "Books"
-    start_urls = ["https://www.amazon.in/si=stripbooks&ref=nb_sb_noss"]
+    start_urls = ["https://www.amazon.in/s?i=stripbooks&ref=nb_sb_noss"]
 
     def parse(self, response):
-        print(response.url)
+        # print(response.url)
+        page_title = response.css("title::text").extract_first()
+        yield {"page_title": page_title}
+        book_details = response.css("span.a-truncate-full::text").extract()
+        current_whole_prices = response.css(
+            "span.a-price-whole::text").extract()
+        all_prices = response.css("span.a-offscreen::text").extract()
