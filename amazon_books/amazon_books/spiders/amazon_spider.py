4ee915952b61bd1f90001a3e94a63eaeff844194
Vignesh P <vignesh.p1110@gmail.com>
2020-06-27 21:49:45 +0530

    sample xpaths

--- amazon_books/amazon_books/spiders/amazon_spider.py
+++ amazon_books/amazon_books/spiders/amazon_spider.py
@@ -2,14 +2,17 @@ import scrapy
 
 
 class AmazonSpider(scrapy.Spider):
-    name = "Books"
+    name = "books"
     start_urls = ["https://www.amazon.in/s?i=stripbooks&ref=nb_sb_noss"]
 
     def parse(self, response):
         # print(response.url)
-        page_title = response.css("title::text").extract_first()
-        yield {"page_title": page_title}
-        book_details = response.css("span.a-truncate-full::text").extract()
+        page_title_css = response.css("title::text").extract_first()
+        page_title_xpath = response.xpath("//title/text()").extract()
+        yield {"page_title": [page_title_css, page_title_xpath]}
+        book_details_css = response.css("span.a-truncate-full::text").extract()
+        book_details_xpath = response.xpath(
+            "//span[@class='a-truncate-cut']/text()").extract()
         current_whole_prices = response.css(
             "span.a-price-whole::text").extract()
         all_prices = response.css("span.a-offscreen::text").extract()
