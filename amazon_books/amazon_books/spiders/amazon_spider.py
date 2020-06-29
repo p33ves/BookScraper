dcc1320fe266a38df9fe1886bdd8334f9a064170
Vignesh P <vignesh.p1110@gmail.com>
2020-06-29 23:37:24 +0530

    cleaned up AmazonBooksItem yields

--- amazon_books/amazon_books/spiders/amazon_spider.py
+++ amazon_books/amazon_books/spiders/amazon_spider.py
@@ -22,26 +22,27 @@ class AmazonSpider(scrapy.Spider):
         all_books = response.css("li.a-carousel-card")
         for book in all_books:
             book_title = book.css(
-                ".acs-product-block__product-title").css("span.a-truncate-full::text").extract()
+                ".acs-product-block__product-title").css("span.a-truncate-full::text").extract()[0]
             book_author = book.css(
-                "span.acs-product-block__contributor").css("span.a-truncate-full::text").extract()
+                "span.acs-product-block__contributor").css("span.a-truncate-full::text").extract()[0]
             book_binding = book.css(
-                "span.acs-product-block__binding-value::text").extract()
+                "span.acs-product-block__binding-value::text").extract()[0]
             price = book.css("div.acs-product-block__price")
             current_price = price.css(
-                "span.acs-product-block__price--buying").css("span.a-offscreen::text").extract()
+                "span.acs-product-block__price--buying").css("span.a-offscreen::text").extract()[0]
             listed_price = price.css(
-                "span.acs-product-block__price--strikethrough").css("span.a-offscreen::text").extract()
+                "span.acs-product-block__price--strikethrough").css("span.a-offscreen::text").extract()[0]
             book_rating = book.css("div.acs-product-block__rating")
-            book_stars = book_rating.css("i").extract()
+            book_stars = book_rating.css("i")[0].xpath(
+                "@class").extract()[0]
             book_review_count = book_rating.css(
-                "span.acs-product-block__rating__review-count::text").extract()
+                "span.acs-product-block__rating__review-count::text").extract()[0]
 
             item['title'] = book_title
-            item['author'] = book_author
+            item['author'] = ' '.join(book_author.split())
             item['binding'] = book_binding
             item['current_price'] = current_price
             item['listed_price'] = listed_price
-            item['rating'] = book_stars
-            item['review_count'] = book_review_count
+            item['rating'] = book_stars.split()[2][14:].replace('-', '.')
+            item['review_count'] = book_review_count[1:-1]
             yield item
